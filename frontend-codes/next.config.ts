01c26e133274b6d14fc8c396ff1f1e5286e74877
kennethodoh <kennethodoh30@gmail.com>
2026-02-10 00:32:52 +0100

    Fixed nextconfig

--- frontend-codes/next.config.ts
+++ frontend-codes/next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
 
   env: {
     NEXT_PUBLIC_API_BASE_URL:
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
   },
 };
 
