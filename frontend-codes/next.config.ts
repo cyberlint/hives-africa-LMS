90c53b8cf98f350f74f2276faf547d5f635909a2
kennethodoh <kennethodoh30@gmail.com>
2026-02-10 02:24:02 +0100

    Added bufferutil and utf-8-validate as external packages

--- frontend-codes/next.config.ts
+++ frontend-codes/next.config.ts
@@ -1,3 +1,31 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   transpilePackages: ["better-auth"],
+//   experimental: {
+//     serverMinification: false,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "nexthive-lms.fly.storage.tigris.dev",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+
+//   serverExternalPackages: ["prettier"],
+
+//   env: {
+//     NEXT_PUBLIC_API_BASE_URL:
+//       process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -15,7 +43,8 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  serverExternalPackages: ["prettier"],
+  // Add bufferutil and utf-8-validate here
+  serverExternalPackages: ["prettier", "bufferutil", "utf-8-validate"],
 
   env: {
     NEXT_PUBLIC_API_BASE_URL:
